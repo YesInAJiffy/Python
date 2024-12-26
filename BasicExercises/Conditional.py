47ef096124f739a519e9cd48185e8363fb9213c2
@NurtureLearning <121309018+YesInAJiffy@users.noreply.github.com>
2024-12-26 14:49:33 -0500

    Create Conditional.py

---
+++ BasicExercises/Conditional.py
@@ -0,0 +1,11 @@
+elderbrother = "Saatvik"
+youngerbrother = "Shlok"
+
+x = input("What is your age ");
+
+if 9 in x:
+  print("Welcome ", youngerbrother)
+elif 14 in x:
+  print("Welcome ", elderbrother)
+else:
+  print("Welcome Man).
